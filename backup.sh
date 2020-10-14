de3c1cc219511bc0a12c8e76bae8c6a2969200e9
gryczanu <52965471+gryczanu@users.noreply.github.com>
2020-10-14 14:11:01 +0200

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -2,14 +2,15 @@
 
 set -e
 
+echo "creating backup: $(date)"
+
 echo "Job started: $(date)"
+DATABASE_HOST=${DATABASE_HOST:-localhost}
+DB_PORT=${DB_PORT:-27017}
 
 DATE=$(date +%Y%m%d_%H%M%S)
-FILE="/data/mongodump/backup-$DATE.tar.gz"
+FILE="/home/urszula/test/Mongo/backup-$DATE.tar.gz"
 
-mkdir -p dump
-mongodump -h $MONGO_HOST -p $MONGO_PORT
-tar -zcvf $FILE dump/
-rm -rf dump/
+mongodump --host $DATABASE_HOST:$DB_PORT --gzip --archive=$FILE
 
-echo "Job finished: $(date)"
+echo "backup created: $(date)"
